a5b54b0224f70d30759052e1f3f15742dada0d4a
Astik Shah <shahastik123@gmail.com>
2025-06-19 03:53:11 +0530

    Convert hero setcion into React component

---
+++ portfolio-using-react/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import './Hero.css';
+export function Hero(){
+    return(
+        
+    <section id="home" class="hero">
+      <div class="hero-content">
+        <div class="hero-text">
+          <h1 class="animate-title">Welcome to Astik's Creative Hub</h1>
+          <p class="animate-subtitle">
+            Where web design meets AI, ML, and ideas come to life!
+          </p>
+          <a href="#projects" class="btn animate-button">Explore Projects</a>
+        </div>
+      </div>
+    </section>
+
+
+
+    );
+}
